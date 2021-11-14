a6e8e67b9fac611966b0b691664e2a11c9803621
Elnatan999 <84679871+Elnatan999@users.noreply.github.com>
2021-11-14 17:37:15 +0100

    test

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-A test file to test the console for the hbnb website
+A test file to test the console for the hbnb project
 """
 
 import unittest
+with patch('sys.stdout', new=StringIO()) as f:
+    HBNBCommand().onecmd("help show")
